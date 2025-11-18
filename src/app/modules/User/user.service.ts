4c1beb51a2ab7ed5523b4769ecfdc05b9972b899
abrarasif11 <faasif929@gmail.com>
2025-11-18 20:08:15 +0600

    update migration

--- src/app/modules/User/user.service.ts
+++ src/app/modules/User/user.service.ts
@@ -1,4 +1,11 @@
-const createAdmin = async () => {
+import { UserRole } from "@prisma/client";
+
+const createAdmin = async (data: any) => {
+  const userData = {
+    email: data.admin.email,
+    password: data.password,
+    role: UserRole.ADMIN,
+  };
   return {
     message: "Admin Created",
   };
