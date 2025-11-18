1bb279faae19d6f2bc73060c179c3a5bec19f7e3
abrarasif11 <faasif929@gmail.com>
2025-11-18 19:57:05 +0600

    create resource

--- src/app.ts
+++ src/app.ts
@@ -5,6 +5,10 @@ import { useRoutes } from "./app/modules/User/user.routes.js";
 const app: Application = express();
 app.use(cors());
 
+// Parser
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
 app.get("/", (req: Request, res: Response) => {
   res.send({
     Message: "PH Health Care Server...",
