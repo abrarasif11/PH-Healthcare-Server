cf27c6199501b6fe76c4a3ba0d07b94119f81b4b
abrarasif11 <faasif929@gmail.com>
2025-11-19 20:14:57 +0600

    Exception Handling

--- src/app/modules/User/user.controller.ts
+++ src/app/modules/User/user.controller.ts
@@ -1,14 +1,23 @@
 import { Request, Response } from "express";
 import { userService } from "./user.service.js";
+import { error } from "console";
 
 const createAdmin = async (req: Request, res: Response) => {
-  // console.log(req.body);
-  const result = await userService.createAdmin(req.body);
-  res.status(200).json({
-    success: true,
-    message: "Admin Created Successfully",
-    data: result,
-  });
+  try {
+    // console.log(req.body);
+    const result = await userService.createAdmin(req.body);
+    res.status(200).json({
+      success: true,
+      message: "Admin Created Successfully",
+      data: result,
+    });
+  } catch (err) {
+    res.status(500).json({
+      success: false,
+      message: err?.name || "Something Went Wrong",
+      error: err,
+    });
+  }
 };
 export const userController = {
   createAdmin,
