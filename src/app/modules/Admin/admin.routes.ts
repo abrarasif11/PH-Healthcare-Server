43b893f7fd5e93519912d6e7f4c13e2428a7f035
abrarasif11 <faasif929@gmail.com>
2025-11-19 23:09:25 +0600

    Implementing Search

--- src/app/modules/Admin/admin.routes.ts
+++ src/app/modules/Admin/admin.routes.ts
@@ -1,4 +1,4 @@
-import express, { Request, Response } from "express";
+import express from "express";
 import { AdminController } from "./admin.controller.js";
 
 const router = express.Router();
--- src/app/modules/Admin/admin.service.ts
+++ src/app/modules/Admin/admin.service.ts
@@ -1,7 +1,14 @@
 import { PrismaClient } from "@prisma/client";
 const prisma = new PrismaClient();
-const getAllFromDB = async () => {
-  const result = await prisma.admin.findMany();
+const getAllFromDB = async (params: any) => {
+  console.log({ params });
+  const result = await prisma.admin.findMany({
+    where: {
+      name: {
+        contains: params.searchTerm,
+      },
+    },
+  });
   return result;
 };
 
