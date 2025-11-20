f2d5353f03beb105a326afcc6fd6c5d04ac86ba1
abrarasif11 <faasif929@gmail.com>
2025-11-20 22:37:00 +0600

    Filtering on Specific Fields

--- src/app/modules/Admin/admin.service.ts
+++ src/app/modules/Admin/admin.service.ts
@@ -1,11 +1,13 @@
 import { Prisma, PrismaClient } from "@prisma/client";
 const prisma = new PrismaClient();
 const getAllFromDB = async (params: any) => {
+  const { searchTerm, ...filterData } = params;
   const andCondition: Prisma.AdminWhereInput[] = [];
-
+  const adminSearchAbleField = ["name", "email"];
+  // console.log(filterData);
   if (params.searchTerm) {
     andCondition.push({
-      OR: ["name", "email"].map((f) => ({
+      OR: adminSearchAbleField.map((f) => ({
         [f]: {
           contains: params.searchTerm,
           mode: "insensitive",
@@ -13,7 +15,18 @@ const getAllFromDB = async (params: any) => {
       })),
     });
   }
-  console.dir(andCondition, { depth: "infinity" });
+
+  if (Object.keys(filterData).length > 0) {
+    andCondition.push({
+      AND: Object.keys(filterData).map((key) => ({
+        [key]: {
+          equals: filterData[key],
+        },
+      })),
+    });
+  }
+
+  // console.dir(andCondition, { depth: "infinity" });
   const whereConditions: Prisma.AdminWhereInput = { AND: andCondition };
   const result = await prisma.admin.findMany({
     where: whereConditions,
