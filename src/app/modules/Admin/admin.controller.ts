bbcdfa51bbf4908b7cefb109cfe881c2fa3ac63b
abrarasif11 <faasif929@gmail.com>
2025-11-22 10:36:02 +0600

    Using the pick Function

--- src/app/modules/Admin/admin.controller.ts
+++ src/app/modules/Admin/admin.controller.ts
@@ -1,11 +1,18 @@
 import { Request, Response } from "express";
 import { AdminService } from "./admin.service.js";
+import pick from "../../../pick.js";
 
 const getAllFromDB = async (req: Request, res: Response) => {
   try {
     // console.log(req.query);
-    const result = await AdminService.getAllFromDB(req.query);
 
+    const filters = pick(req.query, [
+      "name",
+      "email",
+      "searchTerm",
+      "contactNumber",
+    ]);
+    const result = await AdminService.getAllFromDB(filters);
     res.status(200).json({
       success: true,
       message: "Admin Data Fetched",
