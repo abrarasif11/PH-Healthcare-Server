6ccf95fff1b7f9b398e95d8809167ec0b8bae4f9
abrarasif11 <faasif929@gmail.com>
2025-11-23 22:45:34 +0600

    use middleware error

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,7 @@
-import express, { Application, Request, Response } from "express";
+import express, { Application, NextFunction, Request, Response } from "express";
 import cors from "cors";
 import router from "./app/routes/index.js";
+import httpStatus from "http-status";
 
 const app: Application = express();
 app.use(cors());
@@ -16,5 +17,13 @@ app.get("/", (req: Request, res: Response) => {
 });
 
 app.use("/api/v1", router);
+
+app.use((err: any, req: Request, res: Response, next: NextFunction) => {
+  res.status(httpStatus.INTERNAL_SERVER_ERROR).json({
+    success: false,
+    message: err.name || "Something Went Wrong",
+    error: err,
+  });
+});
 
 export default app;
