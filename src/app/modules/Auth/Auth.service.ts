fc7b4cf4207ba6225d77071b94984545bbb7f73c
abrarasif11 <faasif929@gmail.com>
2025-11-25 22:36:08 +0600

    use jwt

--- src/app/modules/Auth/Auth.service.ts
+++ src/app/modules/Auth/Auth.service.ts
@@ -1,6 +1,7 @@
 import prisma from "../../../shared/prisma.js";
 import * as bcrypt from "bcrypt";
-
+import jwt from "jsonwebtoken";
+import { email } from "zod";
 const loginUser = async (payload: { email: string; password: string }) => {
   const userData = await prisma.user.findUniqueOrThrow({
     where: {
@@ -8,12 +9,24 @@ const loginUser = async (payload: { email: string; password: string }) => {
     },
   });
 
-  const isCorrectPass = await bcrypt.compare(
+  const isCorrectPass: boolean = await bcrypt.compare(
     payload.password,
     userData.password
   );
   console.log(isCorrectPass);
 
+  const accessToken = jwt.sign(
+    {
+      email: userData.email,
+      role: userData.role,
+    },
+    "abcdefg",
+    {
+      algorithm: "HS256",
+      expiresIn: "15m",
+    }
+  );
+  console.log("accessToken : ", accessToken);
   return userData;
 };
 export const AuthServices = {
