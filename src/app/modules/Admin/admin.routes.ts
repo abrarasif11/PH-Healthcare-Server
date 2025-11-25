461f91f25e85bfa038830a78cb609132adddfa74
abrarasif11 <faasif929@gmail.com>
2025-11-25 20:36:36 +0600

    use zod

--- src/app/modules/Admin/admin.routes.ts
+++ src/app/modules/Admin/admin.routes.ts
@@ -1,15 +1,34 @@
 import express, { Response, Request, NextFunction } from "express";
 import { AdminController } from "./admin.controller.js";
 
-const validateRequest = (req: Request, res: Response, next: NextFunction) => {
-  console.log("Data", req.body);
-  next();
-};
+import { z } from "zod";
+import { AnyZodObject } from "zod/v3";
+
+const validateRequest =
+  (schema: AnyZodObject) =>
+  async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      await schema.parseAsync({
+        body: req.body,
+      });
+      next();
+    } catch (err) {
+      next(err);
+    }
+  };
+
+const update = z.object({
+  body: z.object({
+    name: z.string().optional(),
+    contactNumber: z.string().optional(),
+  }),
+});
 
 const router = express.Router();
+
 router.get("/", AdminController.getAllFromDB);
 router.get("/:id", AdminController.getIdByDb);
-router.patch("/:id", validateRequest, AdminController.updateIntoDB);
+router.patch("/:id", validateRequest(update), AdminController.updateIntoDB);
 router.delete("/:id", AdminController.deleteFromDb);
 router.delete("/soft/:id", AdminController.softDeleteFromDb);
 export const AdminRoutes = router;
