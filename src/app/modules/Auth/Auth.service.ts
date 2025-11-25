7c9b4cc9e75cdac0e47cdb4f2a02e303ee3bd71c
abrarasif11 <faasif929@gmail.com>
2025-11-25 22:22:11 +0600

    is correct pass

--- src/app/modules/Auth/Auth.service.ts
+++ src/app/modules/Auth/Auth.service.ts
@@ -1,5 +1,20 @@
+import prisma from "../../../shared/prisma.js";
+import * as bcrypt from "bcrypt";
+
 const loginUser = async (payload: { email: string; password: string }) => {
-  console.log("User Login", payload);
+  const userData = await prisma.user.findUniqueOrThrow({
+    where: {
+      email: payload.email,
+    },
+  });
+
+  const isCorrectPass = await bcrypt.compare(
+    payload.password,
+    userData.password
+  );
+  console.log(isCorrectPass);
+
+  return userData;
 };
 export const AuthServices = {
   loginUser,
