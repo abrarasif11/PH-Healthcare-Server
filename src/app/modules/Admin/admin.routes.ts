5820416f93f2a85cb3aca45274638e7448a1834d
abrarasif11 <faasif929@gmail.com>
2025-11-25 20:16:54 +0600

    request validate middleware

--- src/app/modules/Admin/admin.routes.ts
+++ src/app/modules/Admin/admin.routes.ts
@@ -1,10 +1,15 @@
-import express from "express";
+import express, { Response, Request, NextFunction } from "express";
 import { AdminController } from "./admin.controller.js";
+
+const validateRequest = (req: Request, res: Response, next: NextFunction) => {
+  console.log("Data", req.body);
+  next();
+};
 
 const router = express.Router();
 router.get("/", AdminController.getAllFromDB);
 router.get("/:id", AdminController.getIdByDb);
-router.patch("/:id", AdminController.updateIntoDB);
+router.patch("/:id", validateRequest, AdminController.updateIntoDB);
 router.delete("/:id", AdminController.deleteFromDb);
 router.delete("/soft/:id", AdminController.softDeleteFromDb);
 export const AdminRoutes = router;
