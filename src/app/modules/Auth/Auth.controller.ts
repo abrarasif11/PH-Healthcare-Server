c3e0c38b210dcdd3762a18fb29a161f5305195a6
abrarasif11 <faasif929@gmail.com>
2025-11-25 22:53:46 +0600

    set up cookie

--- src/app/modules/Auth/Auth.controller.ts
+++ src/app/modules/Auth/Auth.controller.ts
@@ -6,11 +6,21 @@ import { AuthServices } from "./Auth.service.js";
 const loginUser = catchAsync(async (req: Request, res: Response) => {
   const result = await AuthServices.loginUser(req.body);
 
+  const { refreshToken } = result;
+
+  res.cookie("refreshToken", refreshToken, {
+    secure: false,
+    httpOnly: true,
+  });
+
   sendResponse(res, {
     statusCode: httpStatus.OK,
     success: true,
     message: "Login Successful",
-    data: result,
+    data: {
+      accessToken: result.accessToken,
+      needPassChange: result.needPassChange,
+    },
   });
 });
 export const AuthController = {
