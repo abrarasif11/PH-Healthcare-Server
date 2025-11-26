80677aa22fdb1cfa33f2e95141f62b56e336fefe
abrarasif11 <faasif929@gmail.com>
2025-11-26 19:07:03 +0600

    Handling API Errors

---
+++ src/app/middlewares/auth.ts
@@ -0,0 +1,33 @@
+import { NextFunction, Request, Response } from "express";
+import { jwtHelpers } from "../../helpers/jwtHelpers.js";
+import config from "../../config/index.js";
+import { Secret } from "jsonwebtoken";
+import ApiError from "../errors/apiErrors.js";
+import httpStatus from "http-status";
+
+const auth = (...roles: string[]) => {
+  return async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      const token = req.headers.authorization;
+
+      if (!token) {
+        throw new ApiError(httpStatus.UNAUTHORIZED, "You are not authorized");
+      }
+
+      const verifiedUser = jwtHelpers.verifyToken(
+        token,
+        config.jwt.jwt_secret as Secret
+      );
+      console.log(verifiedUser);
+
+      if (roles.length && !roles.includes(verifiedUser.role)) {
+        throw new ApiError(httpStatus.FORBIDDEN, "Forbidden Access");
+      }
+      next();
+    } catch (err) {
+      next(err);
+    }
+  };
+};
+
+export default auth;
--- src/app/modules/User/user.routes.ts
+++ src/app/modules/User/user.routes.ts
@@ -1,35 +1,8 @@
-import express, { NextFunction, Request, Response } from "express";
+import express from "express";
 import { userController } from "./user.controller.js";
-import { jwtHelpers } from "../../../helpers/jwtHelpers.js";
-import config from "../../../config/index.js";
-import { Secret } from "jsonwebtoken";
+import auth from "../../middlewares/auth.js";
 
 const router = express.Router();
-
-const auth = (...roles: string[]) => {
-  return async (req: Request, res: Response, next: NextFunction) => {
-    try {
-      const token = req.headers.authorization;
-
-      if (!token) {
-        throw new Error("You are authorized");
-      }
-
-      const verifiedUser = jwtHelpers.verifyToken(
-        token,
-        config.jwt.jwt_secret as Secret
-      );
-      console.log(verifiedUser);
-
-      if (roles.length && !roles.includes(verifiedUser.role)) {
-        throw new Error("You are authorized");
-      }
-      next();
-    } catch (err) {
-      next(err);
-    }
-  };
-};
 
 router.post("/", auth("ADMIN", "SUPER_ADMIN"), userController.createAdmin);
 
