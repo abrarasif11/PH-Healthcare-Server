da6ef013192b93443f1abb6169e2f1b4c23e5586
abrarasif11 <faasif929@gmail.com>
2025-11-26 19:22:04 +0600

    Access Control on API Routes

--- src/app/modules/Admin/admin.routes.ts
+++ src/app/modules/Admin/admin.routes.ts
@@ -3,16 +3,35 @@ import { AdminController } from "./admin.controller.js";
 
 import validateRequest from "../../middlewares/validateReq.js";
 import { adminValidateSchemas } from "./admin.validation.js";
+import auth from "../../middlewares/auth.js";
+import { UserRole } from "@prisma/client";
 
 const router = express.Router();
 
-router.get("/", AdminController.getAllFromDB);
-router.get("/:id", AdminController.getIdByDb);
+router.get(
+  "/",
+  auth(UserRole.SUPER_ADMIN, UserRole.ADMIN),
+  AdminController.getAllFromDB
+);
+router.get(
+  "/:id",
+  auth(UserRole.SUPER_ADMIN, UserRole.ADMIN),
+  AdminController.getIdByDb
+);
 router.patch(
   "/:id",
+  auth(UserRole.SUPER_ADMIN, UserRole.ADMIN),
   validateRequest(adminValidateSchemas.update),
   AdminController.updateIntoDB
 );
-router.delete("/:id", AdminController.deleteFromDb);
-router.delete("/soft/:id", AdminController.softDeleteFromDb);
+router.delete(
+  "/:id",
+  auth(UserRole.SUPER_ADMIN, UserRole.ADMIN),
+  AdminController.deleteFromDb
+);
+router.delete(
+  "/soft/:id",
+  auth(UserRole.SUPER_ADMIN, UserRole.ADMIN),
+  AdminController.softDeleteFromDb
+);
 export const AdminRoutes = router;
