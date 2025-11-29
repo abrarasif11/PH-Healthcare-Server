8c44fc492d9059779a01463d958b98e12e542c05
abrarasif11 <faasif929@gmail.com>
2025-11-29 23:52:28 +0600

    Updating Doctor Specialties

--- src/app/modules/Doctor/Doctor.service.ts
+++ src/app/modules/Doctor/Doctor.service.ts
@@ -19,14 +19,34 @@ const updateIntoDB = async (id: string, payload: any) => {
       },
     });
 
-    for (const specialtiesId of specialties) {
-      const createDoctorSpecialties =
-        await transactionClient.doctorSpecialties.create({
-          data: {
-            doctorId: doctorInfo.id,
-            specialtiesId: specialtiesId,
-          },
-        });
+    if (specialties && specialties.length > 0) {
+      // delete specialties
+      const deleteSpecialtiesIds = specialties.filter(
+        (specialty) => specialty.isDeleted
+      );
+      for (const specialty of deleteSpecialtiesIds) {
+        const createDoctorSpecialties =
+          await transactionClient.doctorSpecialties.deleteMany({
+            where: {
+              doctorId: doctorInfo.id,
+              specialtiesId: specialty.specialtiesId,
+            },
+          });
+      }
+
+      //  create specialties
+      const createSpecialtiesIds = specialties.filter(
+        (specialty) => !specialty.isDeleted
+      );
+      for (const specialty of createSpecialtiesIds) {
+        const createDoctorSpecialties =
+          await transactionClient.doctorSpecialties.create({
+            data: {
+              doctorId: doctorInfo.id,
+              specialtiesId: specialty.specialtiesId,
+            },
+          });
+      }
     }
 
     return updateDoctorData;
