5da9108f182344d4c1c3f21968be28f9a73e6eeb
abrarasif11 <faasif929@gmail.com>
2025-11-30 22:00:28 +0600

    doctor interface

--- src/app/modules/Doctor/Doctor.controller.ts
+++ src/app/modules/Doctor/Doctor.controller.ts
@@ -3,6 +3,8 @@ import sendResponse from "../../../shared/sendResponse.js";
 import { DoctorService } from "./Doctor.service.js";
 import httpStatus from "http-status";
 import { Request, Response } from "express";
+import pick from "../../../shared/pick.js";
+import { doctorFilterableFields } from "./Doctor.constant.js";
 
 const updateIntoDB = catchAsync(async (req: Request, res: Response) => {
   const { id } = req.params;
@@ -34,4 +36,5 @@ const getAllFromDB = catchAsync(async (req: Request, res: Response) => {
 
 export const DoctorController = {
   updateIntoDB,
+  getAllFromDB,
 };
--- src/app/modules/Doctor/Doctor.service.ts
+++ src/app/modules/Doctor/Doctor.service.ts
@@ -1,3 +1,4 @@
+import { Prisma } from "@prisma/client";
 import prisma from "../../../shared/prisma.js";
 
 const updateIntoDB = async (id: string, payload: any) => {
@@ -64,6 +65,96 @@ const updateIntoDB = async (id: string, payload: any) => {
   return result;
 };
 
+const getAllFromDB = async (
+  filters: IDoctorFilterRequest,
+  options: IPaginationOptions
+) => {
+  const { limit, page, skip } = paginationHelper.calculatePagination(options);
+  const { searchTerm, specialties, ...filterData } = filters;
+
+  const andConditions: Prisma.DoctorWhereInput[] = [];
+
+  if (searchTerm) {
+    andConditions.push({
+      OR: doctorSearchableFields.map((field) => ({
+        [field]: {
+          contains: searchTerm,
+          mode: "insensitive",
+        },
+      })),
+    });
+  }
+
+  // doctor > doctorSpecialties > specialties -> title
+
+  if (specialties && specialties.length > 0) {
+    andConditions.push({
+      doctorSpecialties: {
+        some: {
+          specialities: {
+            title: {
+              contains: specialties,
+              mode: "insensitive",
+            },
+          },
+        },
+      },
+    });
+  }
+
+  if (Object.keys(filterData).length > 0) {
+    const filterConditions = Object.keys(filterData).map((key) => ({
+      [key]: {
+        equals: (filterData as any)[key],
+      },
+    }));
+    andConditions.push(...filterConditions);
+  }
+
+  andConditions.push({
+    isDeleted: false,
+  });
+
+  const whereConditions: Prisma.DoctorWhereInput =
+    andConditions.length > 0 ? { AND: andConditions } : {};
+
+  const result = await prisma.doctor.findMany({
+    where: whereConditions,
+    skip,
+    take: limit,
+    orderBy:
+      options.sortBy && options.sortOrder
+        ? { [options.sortBy]: options.sortOrder }
+        : { averageRating: "desc" },
+    include: {
+      doctorSpecialties: {
+        include: {
+          specialities: true,
+        },
+      },
+      review: {
+        select: {
+          rating: true,
+        },
+      },
+    },
+  });
+
+  const total = await prisma.doctor.count({
+    where: whereConditions,
+  });
+
+  return {
+    meta: {
+      total,
+      page,
+      limit,
+    },
+    data: result,
+  };
+};
+
 export const DoctorService = {
   updateIntoDB,
+  getAllFromDB,
 };
