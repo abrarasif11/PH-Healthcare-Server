782fc85514cd2982191e9e38fe22153408ee41bc
abrarasif11 <faasif929@gmail.com>
2025-12-01 22:22:04 +0600

    Creating Schedule

--- src/app/modules/Schedule/Schedule.service.ts
+++ src/app/modules/Schedule/Schedule.service.ts
@@ -1,4 +1,26 @@
-const insertIntoDB = async (payload: any) => {};
+import { addHours } from "date-fns";
+
+const insertIntoDB = async (payload: any) => {
+  const { startDate, endDate, startTime, endTime } = payload;
+
+  const currentDate = new Date(startDate);
+  const lastDate = new Date(endDate);
+
+  while (currentDate <= lastDate) {
+    const startDateTime = addHours(
+      new Date(currentDate),
+      Number(startTime.split(":")[0])
+    );
+    console.log(startDateTime);
+
+    const endDateTime = addHours(
+      new Date(lastDate),
+      Number(endTime.split(":")[0])
+    );
+
+    while (startDateTime <= endDateTime) {}
+  }
+};
 
 export const ScheduleService = {
   insertIntoDB,
