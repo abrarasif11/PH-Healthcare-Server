4e9a21d45ff8a18004e00f7907f64a99bf351251
abrarasif11 <faasif929@gmail.com>
2025-12-02 10:20:00 +0600

    use data fns

--- src/app/modules/Schedule/Schedule.service.ts
+++ src/app/modules/Schedule/Schedule.service.ts
@@ -1,24 +1,36 @@
-import { addHours } from "date-fns";
+import { addHours, addMinutes, format } from "date-fns";
 
 const insertIntoDB = async (payload: any) => {
   const { startDate, endDate, startTime, endTime } = payload;
+
+  const intervalTime = 30;
 
   const currentDate = new Date(startDate);
   const lastDate = new Date(endDate);
 
   while (currentDate <= lastDate) {
-    const startDateTime = addHours(
-      new Date(currentDate),
-      Number(startTime.split(":")[0])
+    const startDateTime = new Date(
+      addHours(
+        `${format(currentDate, "yyyy-MM-dd")}`,
+        Number(startTime.split(":")[0])
+      )
     );
     console.log(startDateTime);
 
-    const endDateTime = addHours(
-      new Date(lastDate),
-      Number(endTime.split(":")[0])
+    const endDateTime = new Date(
+      addHours(
+        `${format(currentDate, "yyyy-MM-dd")}`,
+        Number(endTime.split(":")[0])
+      )
     );
 
-    while (startDateTime <= endDateTime) {}
+    while (startDateTime < endDateTime) {
+      const scheduleData = {
+        startDateTime: startDateTime,
+        endDateTime: addMinutes(startDateTime, intervalTime),
+      };
+      console.log(scheduleData);
+    }
   }
 };
 
