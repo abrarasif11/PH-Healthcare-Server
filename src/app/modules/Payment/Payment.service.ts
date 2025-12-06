51b02785b347235ed8ee885fd187d64eacacffd0
abrarasif11 <faasif929@gmail.com>
2025-12-06 12:16:25 +0600

    use throw error

--- src/app/modules/Payment/Payment.service.ts
+++ src/app/modules/Payment/Payment.service.ts
@@ -1,6 +1,5 @@
-import axios from "axios";
-import config from "../../../config/index.js";
 import prisma from "../../../shared/prisma.js";
+import { SSLService } from "../SSL/SSL.service.js";
 
 const initPayment = async (appointmentId: string) => {
   const paymentData = await prisma.payment.findFirstOrThrow({
@@ -15,48 +14,19 @@ const initPayment = async (appointmentId: string) => {
       },
     },
   });
-  console.log(paymentData);
 
-  const data = {
-    store_id: config.ssl.storeId,
-    store_passwd: config.ssl.storePass,
-    total_amount: paymentData.amount,
-    currency: "BDT",
-    tran_id: paymentData.transactionId,
-    success_url: config.ssl.successURL,
-    fail_url: config.ssl.failURL,
-    cancel_url: config.ssl.cancelURL,
-    ipn_url: "http://localhost:3030/ipn",
-    shipping_method: "Courier",
-    product_name: "Appointment.",
-    product_category: "Service",
-    product_profile: "general",
-    cus_name: paymentData.appointment.patient.name,
-    cus_email: paymentData.appointment.patient.email,
-    cus_add1: paymentData.appointment.patient.address,
-    cus_add2: "N/A",
-    cus_city: "Dhaka",
-    cus_state: "Dhaka",
-    cus_postcode: "1000",
-    cus_country: "Bangladesh",
-    cus_phone: paymentData.appointment.patient.contactNumber,
-    cus_fax: "01711111111",
-    ship_name: "N/A",
-    ship_add1: "N/A",
-    ship_add2: "N/A",
-    ship_city: "N/A",
-    ship_state: "N/A",
-    ship_postcode: 1000,
-    ship_country: "N/A",
+  const initPaymentData = {
+    amount: paymentData.amount,
+    transactionId: paymentData.transactionId,
+    name: paymentData.appointment.patient.name,
+    email: paymentData.appointment.patient.email,
+    address: paymentData.appointment.patient.address,
+    phoneNumber: paymentData.appointment.patient.contactNumber,
   };
-
-  //   const response = await axios({
-  //     method: "post",
-  //     url: config.ssl.sslPaymentAPI,
-  //     data: data,
-  //     headers: { "Content-Type": "application/x-www-form-urlencoded" },
-  //   });
-  //   console.log(response.data);
+  const result = await SSLService.initPayment(initPaymentData);
+  return {
+    paymentUrl: result.GatewayPageURL,
+  };
 };
 export const PaymentService = {
   initPayment,
