1e5eaa1a2ebceba4eb07e19b573c4562d1196793
abrarasif11 <faasif929@gmail.com>
2025-12-10 18:41:57 +0600

    getAdminMetaData

--- src/app/modules/Meta/Meta.service.ts
+++ src/app/modules/Meta/Meta.service.ts
@@ -1,5 +1,6 @@
-import { UserRole } from "@prisma/client";
+import { PaymentStatus, UserRole } from "@prisma/client";
 import { IAuthUser } from "../../interfaces/common.js";
+import prisma from "../../../shared/prisma.js";
 
 const fetchDashboardMetaData = async (user: IAuthUser) => {
   let metaData;
@@ -25,7 +26,32 @@ const fetchDashboardMetaData = async (user: IAuthUser) => {
 
 const getSuperAdminMetaData = async (user: IAuthUser) => {};
 
-const getAdminMetaData = async (user: IAuthUser) => {};
+const getAdminMetaData = async (user: IAuthUser) => {
+  const appointmentCount = await prisma.appointment.count();
+  const patientCount = await prisma.patient.count();
+  const doctorCount = await prisma.doctor.count();
+  const paymentCount = await prisma.payment.count();
+
+  const totalRevenue = await prisma.payment.aggregate({
+    _sum: { amount: true },
+    where: {
+      status: PaymentStatus.PAID,
+    },
+  });
+
+  const barChartData = await getBarChartData();
+  const pieCharData = await getPieChartData();
+
+  return {
+    appointmentCount,
+    patientCount,
+    doctorCount,
+    paymentCount,
+    totalRevenue,
+    barChartData,
+    pieCharData,
+  };
+};
 
 const getDoctorMetaData = async (user: IAuthUser) => {};
 
